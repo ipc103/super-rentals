89a6cd1e8d28dbcdb733e9b93528478a9196333a
ipc103 <iancandysemail@gmail.com>
2016-07-15 09:44:53 -0400

    add rental listing component

--- app/components/rental-listing.js
+++ app/components/rental-listing.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  isWide: false,
+  // actions: {
+  //   toggleImageSize(){
+  //     this.toggleProperty('isWide');
+  //   }
+  // },
+  mouseEnter(){
+    this.toggleProperty('isWide');
+  },
+  mouseLeave(){
+    this.toggleProperty('isWide');
+  }
 });
